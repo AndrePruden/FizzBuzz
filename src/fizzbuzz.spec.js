dabe8a7eb0a92f05aafc02c68eb6355504d62364
AndrePruden <andrepruden0@gmail.com>
2024-02-22 10:01:54 -0400

    Si genero para un número sin regla me genera el mismo número

---
+++ src/fizzbuzz.spec.js
@@ -0,0 +1,14 @@
+import generarFizzBuzz from "./fizzbuzz.js";
+
+describe("FizzBuzz", () => {
+  it("deberia generar el mismo numero para uno que no tiene reglas como el nro 1", () => {
+    expect(generarFizzBuzz(1)).toEqual("1");
+  });
+
+  it("deberia generar el mismo numero para otro numero que no tiene reglas como el nro 2", () => {
+    expect(generarFizzBuzz(2)).toEqual("2");
+  });
+
+  
+});
+
