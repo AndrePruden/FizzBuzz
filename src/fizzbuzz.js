1b79677a1abbcae69a8fed62088461cb747fcadf
AndrePruden <andrepruden0@gmail.com>
2024-02-22 10:07:53 -0400

    Si genero un número con la refla del FizzBuzz me genera FizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,6 +2,11 @@ function generarFizzBuzz(n){
     const FIZZ = 3;
     const BUZZ = 5;
 
+    if(n % FIZZ === 0 && n % BUZZ === 0)
+    {
+        return "FizzBuzz";
+    }
+
     if(n % FIZZ === 0)
     {
         return "Fizz";
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -24,5 +24,13 @@ describe("FizzBuzz", () => {
   it("deberia generar Buzz para un numero multiplo de 5", () => {
     expect(generarFizzBuzz(10)).toEqual("Buzz");
   });
+
+  it("deberia generar FizzBuzz para un numero como el nro 15", () => {
+    expect(generarFizzBuzz(15)).toEqual("FizzBuzz");
+  });
+
+  it("deberia generar FizzBuzz para un numero multiplo de 3 y 5", () => {
+    expect(generarFizzBuzz(30)).toEqual("FizzBuzz");
+  });
 });
 
